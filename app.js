22f1086362711059c725c55359435dc594da86fa
Ben <benjamind10@pm.me>
2022-01-29 09:59:50 -0500

    added first title question and tested app in node

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Title of your project? (Required)',
+      validate: validateTitle => {
+        if (validateTitle) return true;
+        else {
+          console.log('Enter a project title');
+          return false;
+        }
+      },
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions();
+}
 
 // Function call to initialize app
 init();
